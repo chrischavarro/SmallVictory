d0c77c1eb4674f0081b7fe09afabd1b5181bc6bc
Christian Chavarro <christian@clutchprep.com>
2016-09-04 01:38:12 -0400

    Integrated Phony gem to validate phone number on user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+validates_plausible_phone :phone_number, presence: true
+validates_plausible_phone :phone_number, with: /\A\+\d+/
+validates_plausible_phone :phone_number, without: /\A\+\d+/
+validates_plausible_phone :phone_number, presence: true, with: /\A\+\d+/
+
+  phony_normalize :phone_number, default_country_code: 'US'
+  phony_normalize :phone_number, as: :phone_number_normalized_version, default_country_code: 'US'
+  phony_normalized_method :fax_number
 end
