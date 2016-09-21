612cb491b3d047ceed19e5219220ff9a2af80d52
Christian Chavarro <christian@clutchprep.com>
2016-09-20 21:58:46 -0400

    User is shown completions and completion percentage on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,17 @@ class DashboardController < ApplicationController
 
 	def main
 		# @user = User.find_by(params[:id])
-		
+		completions = UserTrackCompletionAssociation.where(user_id: current_user.id)
+		@completed = []
+		@attempted = []
+		completions.each do |completion|
+			if completion.completed
+				@completed.push(completion)
+			end
+			@attempted.push(completion)
+		end
+
+		@completion_percentage = (@completed.count.to_f/@attempted.count.to_f) * 100
 	end
 
 	protected
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,11 +7,13 @@ class TasksController < ApplicationController
 		completion = UserTrackCompletionAssociation.new
 		completion.user_id = current_user.id
 		completion.track_id = params[:track_id]
-
+		# This is where to decide how long task took, whether or not it was completed, etc
+		completion.completed = true
 		if completion.save
 			redirect_to dashboard_path
 		else
 			redirect_to :back
 		end
+
 	end
 end
