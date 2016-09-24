fea4405babb04d19f68d9004f7b6eefc322ef3c5
Christian Chavarro <christian@clutchprep.com>
2016-09-23 23:23:58 -0400

    Replaced vendor/assets version of ChartJS with gem version, replaced precompiling TimeCircles by including in application.js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,10 +6,8 @@ Rails.application.config.assets.precompile += %w(moment.min.js)
 Rails.application.config.assets.precompile += %w(jquery.mousewheel.min.js)
 Rails.application.config.assets.precompile += %w(jquery.timepicker.min.css)
 Rails.application.config.assets.precompile += %w(jquery.timepicker.min.js)
-Rails.application.config.assets.precompile += %w(Chart.js)
 Rails.application.config.assets.precompile += %w(justgage.js)
 Rails.application.config.assets.precompile += %w(raphael-2.1.4.min.js)
-Rails.application.config.assets.precompile += %w(TimeCircles.js)
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
